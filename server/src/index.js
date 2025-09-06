656f7cb93f977b5658c96f1217c00f1a311f0fdf
Ashish <ashish.ghildiyal@xelba.io>
2025-09-06 13:14:49 +0530

    Changes

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ dotenv.config({
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-        res.send('API is running');
+        res.send('API is working');
 })
 
 connectDB();

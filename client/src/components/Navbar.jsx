50b90359398488aec09785acb322e58ea89025bd
Ashish <ashish.ghildiyal@xelba.io>
2025-09-08 10:00:24 +0530

    changes in client folder

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,14 +5,14 @@ import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
     const {openSignIn} = useClerk();
-    const {issignedIn, user} = useUser();
+    const {isSignedIn, user} = useUser();
   return (
     <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
     <Link to='/'>
         <img className="w-32 sm:w-44" src={assets.logo} alt="" />
     </Link>
     {
-        issignedIn ? <div><UserButton/> </div>
+        isSignedIn ? <div><UserButton/> </div>
         :  <button onClick={()=>openSignIn({})} className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full">
             Get started{" "}
             <img
